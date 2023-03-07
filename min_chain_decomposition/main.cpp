8add2ec1f61a800bf484252ed292eeaab4a5153b
Dhvanit-Beniwal <beniwal33c@gmail.com>
2023-03-07 06:30:36 +0530

    complete refactor, operator overloading instead of comparators

--- min_chain_decomposition/main.cpp
+++ min_chain_decomposition/main.cpp
@@ -6,66 +6,78 @@
 #define EPSILON 1e-7
 #define CLOSE(a,b) (abs(a-b)<EPSILON)
 
-auto cmp_dot = [](sf::CircleShape a, sf::CircleShape b) {
-    if(!CLOSE(a.getPosition().x, b.getPosition().x)){
-        return a.getPosition().x < b.getPosition().x;
-    }else{
-        return a.getPosition().y > b.getPosition().y;
-    }
-};
-
-auto cmp_chain = [](std::vector<sf::Vertex>* chain1, std::vector<sf::Vertex>* chain2) {
-    return chain1->back().position.y < chain2->back().position.y;
-};
-
-std::set< sf::CircleShape, decltype(cmp_dot) > points(cmp_dot);
-std::set< std::vector<sf::Vertex>* , decltype(cmp_chain) > chains(cmp_chain);
-
 // "dot" referes to a mathematical point but drawn with a thickness, represented as an SFML circle
 // "vertex" refers to just the 1-pixel point, also an SFML vertex
 // programmatically convenient to draw() chain with vertex-array but visually convenient to draw() points with dots
 
-sf::CircleShape make_dot(float x, float y){
-    float r = 2.f;
-    sf::CircleShape point(r, 10);
-    point.setPosition(x-r,y-r);
-    point.setFillColor(sf::Color::White);
-    point.setOutlineThickness(2.f);
-    point.setOutlineColor(sf::Color::Blue);
-    return point;
-}
-sf::Vertex dot_to_vertex(sf::CircleShape dot){
-    float r = dot.getRadius();
-    return sf::Vertex(sf::Vector2f(dot.getPosition().x+r, dot.getPosition().y+r), sf::Color::White);
-}
+class Point : public sf::Drawable{
+        sf::CircleShape dot;
+        void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+            target.draw(dot, states);
+        }
+    public:
+        sf::Vertex vertex;
+        Point(float x, float y){
+            float r = 2.f;
+            dot = sf::CircleShape(r, 10);
+            dot.setPosition(x-r,y-r);
+            dot.setFillColor(sf::Color::White);
+            dot.setOutlineThickness(2.f);
+            dot.setOutlineColor(sf::Color::Blue);
+            vertex = sf::Vertex(sf::Vector2f(x, y), sf::Color::White);
+        }
+        bool operator< (const Point& rhs) const{
+            if(!CLOSE(vertex.position.x, rhs.vertex.position.x)){
+                return vertex.position.x < rhs.vertex.position.x;
+            }else{
+                return vertex.position.y > rhs.vertex.position.y;
+            }
+        }
+};
+
+class Chain : public sf::Drawable{
+        std::vector<sf::Vertex> varray;
+        void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+            target.draw(&varray[0], varray.size(), sf::LineStrip, states);
+        }
+    public:
+        Chain(sf::Vertex first){
+            varray.push_back(first);
+        }
+        void add(Point& p) {
+            varray.push_back(p.vertex);
+        }
+        bool operator< (const Chain& rhs) const{                        // chain < chain
+            return varray.back().position.y < rhs.varray.back().position.y;
+        }
+        bool operator< (const Point& rhs) const{                        // chain < point
+            return varray.back().position.y < rhs.vertex.position.y;
+        }
+        friend bool operator< (const Point& lhs, const Chain& rhs) {    // point < chain
+            return lhs.vertex.position.y < rhs.varray.back().position.y;
+        }
+};
+
+std::set< Point > points;
+std::set< Chain, std::less<> > chains;
 
 void make_chains(){
-    for(auto chain: chains){
-        delete chain;
-    }
     chains.clear();
     for(auto p: points){
         if(chains.empty()){
-            chains.insert(new std::vector<sf::Vertex>{dot_to_vertex(p)});
+            chains.insert(Chain(p.vertex));
             continue;
         }
-        bool valid = false;
-        for(auto it = chains.begin(); it!=chains.end(); ++it){
-            auto& q = (*it)->back();
-            valid = (p.getPosition().y < q.position.y); // don't need to check x coord since points are sorted acc to that already
-            if(valid){
-                auto chain = *it;
-                chains.erase(it);
-                chain->push_back(sf::Vertex(dot_to_vertex(p)));
-                chains.insert(chain);
-                break;
-            }
-        }
-        if(!valid){
-            chains.insert(new std::vector<sf::Vertex>{sf::Vertex(dot_to_vertex(p))});
+
+        auto it = chains.upper_bound(p);    // highest chain able to accept p
+        if(it == chains.end()){
+            chains.insert(Chain(p.vertex));
+        }else{
+            auto chain = *it;
+            chain.add(p);   // should update in-place but std::set doesn't allow it since it doesn't know that in this case order never changes on updating a chain
+            chains.erase(it); chains.insert(chain); // therefore an inefficiency (should only need the one 'log_n' traversal with call to upper_bound() )
         }
     }
-    // for(auto chain : chains){std::cout << chain->size() << std::endl;}
 }
 
 
@@ -82,20 +94,17 @@ int main(){
             if (event.type == sf::Event::Closed){window.close();}
             if (event.type == sf::Event::MouseButtonPressed){
                 if(event.mouseButton.button == sf::Mouse::Left){
-                    points.insert(make_dot(event.mouseButton.x, event.mouseButton.y));
+                    points.insert(Point(event.mouseButton.x, event.mouseButton.y));
                 }else
                 if(event.mouseButton.button == sf::Mouse::Right){
                     make_chains();
                 }
             }
-            if(event.mouseButton.button == sf::Mouse::Middle){
-                points.clear();
-                for(auto chain: chains){
-                    delete chain;
-                }
-                chains.clear();
-            }
             if (event.type == sf::Event::KeyPressed){
+                if(event.key.code == sf::Keyboard::R){
+                    points.clear();
+                    chains.clear();
+                }else
                 if (event.key.code == sf::Keyboard::H){
                     display_chains = !display_chains;
                 }
@@ -103,12 +112,13 @@ int main(){
         }
 
         window.clear(sf::Color::Black);
+        
         for(auto p: points){window.draw(p);}
+        
         if(display_chains){
-            for(auto chain: chains){
-                window.draw(&(*chain)[0], chain->size(), sf::LineStrip);
-            }
+        for(auto c: chains){window.draw(c);}
         }
+        
         window.display();
     }
 
